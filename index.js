355df7da0e7eaae8948561d872e2035f0f2cc5fc
aashish-300 <aashishmahato224@gmail.com>
2021-07-10 02:10:36 +0545

    complete weather for first submission

--- index.js
+++ index.js
@@ -15,12 +15,57 @@ const pressure = document.getElementById('pressure');
 const dewPoint = document.getElementById('dewPoint');
 const cloud = document.querySelector('.cloud');
 
+//5days cloud class
+const cloud1 = document.querySelector('.cloud-1');
+const cloud2 = document.querySelector('.cloud-2');
+const cloud3 = document.querySelector('.cloud-3');
+const cloud4 = document.querySelector('.cloud-4');
+const cloud5 = document.querySelector('.cloud-5');
+
+//5days humidity class 
+const humidity1 = document.querySelector('.humidity1');
+const humidity2 = document.querySelector('.humidity2');
+const humidity3 = document.querySelector('.humidity3');
+const humidity4 = document.querySelector('.humidity4');
+const humidity5 = document.querySelector('.humidity5');
+
+//5days maximum temperature class 
+const maxTemp1 = document.querySelector('.temp__max--1');
+const maxTemp2 = document.querySelector('.temp__max--2');
+const maxTemp3 = document.querySelector('.temp__max--3');
+const maxTemp4 = document.querySelector('.temp__max--4');
+const maxTemp5 = document.querySelector('.temp__max--5');
+
+//5days minimum temperature class 
+const minTemp1 = document.querySelector('.temp__min--1');
+const minTemp2 = document.querySelector('.temp__min--2');
+const minTemp3 = document.querySelector('.temp__min--3');
+const minTemp4 = document.querySelector('.temp__min--4');
+const minTemp5 = document.querySelector('.temp__min--5');
+
+//5days date 
+const day1 = document.querySelector('.day1');
+const day2 = document.querySelector('.day2');
+const day3 = document.querySelector('.day3');
+const day4 = document.querySelector('.day4');
+const day5 = document.querySelector('.day5');
+
+
 const state = {};
+
+window.onload = async() => { 
+    state.query = 'norwich';
+    searchData();
+}
 
 searchForm.addEventListener('submit', async(e) => {
     state.query = searchInput.value;
+    searchInput.value = '';
     console.log(state.query);
+    searchData();
+})
 
+const searchData = async() => { 
     if(state.query){
         try{
             currentTime();
@@ -31,8 +76,7 @@ searchForm.addEventListener('submit', async(e) => {
             console.log(err);
         }
     }
-  
-})
+}
 
 const getResult = async() => {
     console.log(apiKey,"defined");
@@ -49,10 +93,10 @@ const getResult = async() => {
     state.City = data.city.name;
 
     const weatherDaily = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${state.lat}&lon=${state.long}&appid=${apiKey}`).then(e => {return e.json();});
-    console.log(weatherDaily);
+    // console.log(weatherDaily);
    
     //store data in global object state
-    const {current} = weatherDaily;
+    const {current, daily} = weatherDaily;
     // state.weatherDaily = weatherDaily;
     state.CurrentTemp = Math.round(current.temp-273);
     state.WeatherCondtion = current.weather[0].description;
@@ -63,7 +107,34 @@ const getResult = async() => {
     state.Pressure = current.pressure;
     state.DewPoint = Math.round(current.dew_point-273);
     state.Icon = current.weather[0].icon;
-    
+
+    //5days forecast cloud icon
+    state.day1CloudIcon = daily[0].weather[0].icon;
+    state.day2CloudIcon = daily[1].weather[0].icon;
+    state.day3CloudIcon = daily[2].weather[0].icon;
+    state.day4CloudIcon = daily[3].weather[0].icon;
+    state.day5CloudIcon = daily[4].weather[0].icon;
+
+    //5 days humidity data
+    state.day1Humidity = daily[0].humidity;
+    state.day2Humidity = daily[1].humidity;
+    state.day3Humidity = daily[2].humidity;
+    state.day4Humidity = daily[3].humidity;
+    state.day5Humidity = daily[4].humidity;
+
+    //5days max temperature
+    state.day1Max = Math.round(daily[0].temp.max - 273);
+    state.day2Max = Math.round(daily[1].temp.max - 273);
+    state.day3Max = Math.round(daily[2].temp.max - 273);
+    state.day4Max = Math.round(daily[3].temp.max - 273);
+    state.day5Max = Math.round(daily[4].temp.max - 273);
+
+    //5days min temperature
+    state.day1Min = Math.round(daily[0].temp.min - 273);
+    state.day2Min = Math.round(daily[1].temp.min - 273);
+    state.day3Min = Math.round(daily[2].temp.min - 273);
+    state.day4Min = Math.round(daily[3].temp.min - 273);
+    state.day5Min = Math.round(daily[4].temp.min - 273);
 }
 
 const renderResult = () => { 
@@ -96,12 +167,45 @@ const renderResult = () => {
     pressure.textContent = Pressure;
     dewPoint.textContent = DewPoint;
     cloud.src = `http://openweathermap.org/img/wn/${Icon}@2x.png`;
+
+    humidity1.textContent = state.day1Humidity;
+    humidity2.textContent = state.day2Humidity;
+    humidity3.textContent = state.day3Humidity;
+    humidity4.textContent = state.day4Humidity;
+    humidity5.textContent = state.day5Humidity;
+
+    maxTemp1.textContent = state.day1Max;
+    maxTemp2.textContent = state.day2Max;
+    maxTemp3.textContent = state.day3Max;
+    maxTemp4.textContent = state.day4Max;
+    maxTemp5.textContent = state.day5Max;
+
+    minTemp1.textContent = state.day1Min;
+    minTemp2.textContent = state.day2Min;
+    minTemp3.textContent = state.day3Min;
+    minTemp4.textContent = state.day4Min;
+    minTemp5.textContent = state.day5Min;
+
+    day1.textContent = state.Day1;
+    day2.textContent = state.Day2;
+    day3.textContent = state.Day3;
+    day4.textContent = state.Day4;
+    day5.textContent = state.Day5;
+    
 };
 
 const currentTime = () => { 
+    const day = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat','Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
     const date = new Date();
     state.Hour = date.getHours() > 12 ? date.getHours()-12 : date.getHours();
     state.Minute = date.getMinutes();
     date.getHours() > 12 ? state.Timezone = 'PM' : state.Timezone = 'AM';
+    
+    state.Day1 = day[date.getDay()];
+    state.Day2 = day[date.getDay()+1];
+    state.Day3 = day[date.getDay()+2];
+    state.Day4 = day[date.getDay()+3];
+    state.Day5 = day[date.getDay()+4];
 }
+
 console.log(state);
